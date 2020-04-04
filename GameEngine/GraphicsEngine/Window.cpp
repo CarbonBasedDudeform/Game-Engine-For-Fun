cc707abc2e113feaa351014925c4c25934c2b8fc
Dominic Musgrave <imdomdude@gmail.com>
2020-04-04 23:43:17 +0100

    hacky vector ops(forward, normal, cross, magnitude) but nicer camera
    hacked in operations for vectors so the camera moves forward in the forward direction, left and right etc.

--- GameEngine/GraphicsEngine/Window.cpp
+++ GameEngine/GraphicsEngine/Window.cpp
@@ -62,13 +62,46 @@ namespace Graphics
 		Create(title, height, width);
 	}
 
+	float magnitude(const Vector& vec)
+	{
+		return sqrt((vec.x * vec.x) + (vec.y * vec.y) + (vec.z * vec.z));
+	}
+
+	void normalize(Vector& vec)
+	{
+		auto const mag = magnitude(vec);
+		vec.x /= mag;
+		vec.y /= mag;
+		vec.z /= mag;
+	}
+
+	Vector cross(const Vector& a, const Vector& b)
+	{
+		return {
+				a.y * b.z - a.z * b.y,
+				a.z * b.x -a.x * b.z ,
+				a.x*b.y - a.y * b.x
+		};
+	}
+
+	Vector calcForward(const Graphics::IRenderer::Camera& camera)
+	{
+		auto forward = Vector{ camera.eye_x - camera.look_at_x
+							 , camera.eye_y - camera.look_at_y
+							 , camera.eye_z - camera.look_at_z };
+		
+		normalize(forward);
+
+		return forward;
+	}
+
 	void Window::Loop() {
 		MSG msg;
 		ZeroMemory(&msg, sizeof(msg));
 
 		gainput::InputManager manager;
 		
-		manager.SetDisplaySize(800,600);
+		manager.SetDisplaySize(1920,1080);
 		const gainput::DeviceId keyboardId = manager.CreateDevice<gainput::InputDeviceKeyboard>();
 		const gainput::DeviceId mouseId = manager.CreateDevice<gainput::InputDeviceMouse>();
 		const gainput::DeviceId padId = manager.CreateDevice<gainput::InputDevicePad>();
@@ -84,10 +117,11 @@ namespace Graphics
 		map.MapBool(Right, keyboardId, gainput::KeyD);
 		map.MapFloat(VerticalLook, mouseId, gainput::MouseAxisY);
 		map.MapFloat(HorizontalLook, mouseId, gainput::MouseAxisX);
-
+		
+		
 		while (msg.message != WM_QUIT)
 		{
-
+			
 			manager.Update();
 			if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
 			{
@@ -101,43 +135,110 @@ namespace Graphics
 
 				if (map.GetBool(Down))
 				{
-					camera_.eye_z-= 0.01f;
-					camera_.look_at_z -= 0.01f;
+					//calc forward vector
+					//flip it
+					//move that way
+					auto forward = calcForward(camera_);
+
+					forward.x *= 0.01;
+					forward.y *= 0.01;
+					forward.z *= 0.01;
+					
+					camera_.eye_x += forward.x;
+					camera_.eye_y += forward.y;
+					camera_.eye_z += forward.z;
+
+					camera_.look_at_x += forward.x;
+					camera_.look_at_y += forward.y;
+					camera_.look_at_z += forward.z;
 					renderer_->MoveEye(camera_);
 				}
 
 				if (map.GetBool(Up))
 				{
-					camera_.eye_z += 0.01f;
-					camera_.look_at_z += 0.01f;
+					//calc forward vector
+					//move that way
+					auto forward = calcForward(camera_);
+
+					forward.x *= -1;
+					forward.y *= -1;
+					forward.z *= -1;
+					
+					forward.x *= 0.01;
+					forward.y *= 0.01;
+					forward.z *= 0.01;
+					
+					camera_.eye_x += forward.x;
+					camera_.eye_y += forward.y;
+					camera_.eye_z += forward.z;
+
+					camera_.look_at_x += forward.x;
+					camera_.look_at_y += forward.y;
+					camera_.look_at_z += forward.z;
 					renderer_->MoveEye(camera_);
 				}
 
 				if (map.GetBool(Left))
 				{
-					camera_.look_at_x -= 0.01f;
-					camera_.eye_x -= 0.01f;
+					//calc forward vector
+					//rotate it 90 around up vector.
+					//move that way
+					auto const forward = calcForward(camera_);
+					auto const up = Vector{ 0, 1, 0 };
+					auto left = cross(forward, up);
+
+					left.x *= -0.01;
+					left.y *= -0.01;
+					left.z *= -0.01;
+					
+					camera_.eye_x += left.x;
+					camera_.eye_y += left.y;
+					camera_.eye_z += left.z;
+
+					camera_.look_at_x += left.x;
+					camera_.look_at_y += left.y;
+					camera_.look_at_z += left.z;
 					renderer_->MoveEye(camera_);
 				}
 				
 				if (map.GetBool(Right))
 				{
-					camera_.look_at_x += 0.01f;
-					camera_.eye_x += 0.01f;
+					auto const forward = calcForward(camera_);
+					auto const up = Vector{ 0, 1, 0 };
+					auto right = cross(forward, up);
+
+					right.x *= 0.01;
+					right.y *= 0.01;
+					right.z *= 0.01;
+
+					camera_.eye_x += right.x;
+					camera_.eye_y += right.y;
+					camera_.eye_z += right.z;
+
+					camera_.look_at_x += right.x;
+					camera_.look_at_y += right.y;
+					camera_.look_at_z += right.z;
 					renderer_->MoveEye(camera_);
 				}
 
 
-				if (map.GetFloat(VerticalLook))
+				if (map.GetFloatDelta(VerticalLook) != 0.0)
 				{
-					camera_.look_at_y += map.GetFloatDelta(VerticalLook) * 100;
+					auto const delta = map.GetFloatDelta(VerticalLook);
+					camera_.look_at_y += delta * -50;
+					//camera_.look_at_y = std::clamp(camera_.look_at_y, -10.0f, 10.0f);
 					renderer_->MoveEye(camera_);
+					//SetCursorPos(1920 / 2, 1080 / 2);
 				}
 
-				if (map.GetFloat(HorizontalLook))
+				if (map.GetFloatDelta(HorizontalLook) != 0.0)
 				{
-					camera_.look_at_x += map.GetFloatDelta(HorizontalLook) * 100;
+					auto const delta = map.GetFloatDelta(HorizontalLook);
+					camera_.look_at_x += delta * 50;
+					//camera_.look_at_x = std::clamp(camera_.look_at_x, -10.0f, 10.0f);
 					renderer_->MoveEye(camera_);
+					
+					//SetCursorPos(1920 / 2, 1080 / 2);
 				}
 
 				renderer_->Render();
